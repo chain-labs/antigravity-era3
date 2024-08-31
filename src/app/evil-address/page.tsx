4a936658afa155b2cd7ce0f22ebc5973e6cd3560
Pratham <chudasamapratham142@gmail.com>
2024-08-31 10:19:04 +0530

    initialized new pages

---
+++ src/app/evil-address/page.tsx
@@ -0,0 +1,7 @@
+export default function EvilAddressPage() {
+	return (
+		<div>
+			<h1>Evil Address</h1>
+		</div>
+	);
+}
---
+++ src/app/treasury/page.tsx
@@ -0,0 +1,7 @@
+export default function TreasuryPage() {
+	return (
+		<div>
+			<h1>Treasury</h1>
+		</div>
+	);
+}
