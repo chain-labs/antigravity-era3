4f58a7498b8c237a7aa19bd038a95f7f0d208af0
Pratham <chudasamapratham142@gmail.com>
2024-09-09 11:57:03 +0530

    new bg image added

--- src/images.ts
+++ src/images.ts
@@ -15,4 +15,6 @@ export const IMAGEKIT_ICONS = {
 
 export const IMAGEKIT_BACKGROUNDS = {
   MINING_PAGE_ERA_3: `${IMAGEKIT}/mining-page-era-3.png?updatedAt=1722848751526`,
+  TREASURY_1: `${IMAGEKIT}/treasury-1.png`,
+  TREASURY_2: `${IMAGEKIT}/treasury-2.png`,
 };
