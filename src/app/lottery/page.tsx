a945f98d42f829a1a667bb8901e2f4f6fcf264ce
Pratham <chudasamapratham142@gmail.com>
2024-11-20 20:56:44 +0530

    added a table for stats, and removing the hover effect stats

--- src/app/lottery/page.tsx
+++ src/app/lottery/page.tsx
@@ -21,6 +21,7 @@ import SeperateText, {
 } from "@/components/animation/SeperateText";
 import { BACKGROUNDS } from "@/constants";
 import ProgressingStates, { STEPPERS } from "./ProgressingStates";
+import Table from "@/components/html/Table";
 
 export default function LotteryPage() {
   const [lotteryState, setLotteryState] = useState<STEPPERS>({
@@ -120,12 +121,44 @@ export default function LotteryPage() {
     };
   }, [fuelCellsInfoModal]);
 
+  const tableConfig = {
+    header: [
+      <div key="lottery header" className="flex flex-col">
+        Lottery <br />
+        <div
+          style={{
+            opacity: 0,
+          }}
+        >
+          ej3w {/* This is a dummy text to fix the height of the header   */}
+        </div>
+      </div>,
+      <>
+        payout / fuelcell <br /> (in $Dark)
+      </>,
+      <>
+        Winners <br /> (in FuelCells)
+      </>,
+      <>
+        Total won by user <br /> (in $Dark)
+      </>,
+      <>
+        won by user <br /> (in $Dark)
+      </>,
+    ],
+    data: [
+      ["big", 13123, 10, 100, 1000],
+      ["bigger", 12312, 10, 100, 1000],
+      ["biggest", 3223, 10, 100, 1000],
+    ],
+  };
+
   return (
     <div
       style={{
         backgroundImage: `url(${BACKGROUNDS.LOTTERY ?? ""})`,
       }}
-      className="relative flex justify-center items-center min-h-screen bg-center bg-no-repeat pt-[100px]"
+      className="relative flex justify-center items-center min-h-screen bg-center bg-cover bg-no-repeat pt-[130px]"
     >
       <div className="absolute inset-0 bg-gradient-to-b from-[black] via-[#0000] to-[black]"></div>
       <div
@@ -134,7 +167,8 @@ export default function LotteryPage() {
           "lg:flex lg:flex-row lg:justify-start lg:items-start gap-[30px]",
         )}
       >
-        <div className="flex flex-col gap-[8px] mx-[16px] md:mx-0">
+        {/* Left side */}
+        <div className="flex flex-col gap-[8px] w-full">
           <div
             className={cn(
               "flex flex-col justify-start items-start gap-[8px]",
@@ -189,13 +223,20 @@ export default function LotteryPage() {
               </div>
             </div>
           </div>
+          <Table
+            header={tableConfig.header}
+            body={tableConfig.data}
+            className="hidden lg:block lg:w-full max-w-full"
+            bodyClassName="text-center flex justify-center items-center"
+          />
         </div>
 
-        <div className="flex flex-col justify-center items-center gap-[24px]">
+        {/* Right side */}
+        <div className="lg:w-[400px] flex flex-col justify-center items-center gap-[24px] mb-[16px] lg:mb-0 w-full">
           <form
             className={cn(
               "flex flex-col justify-center items-center gap-[8px] ",
-              "w-full md:w-[400px]",
+              "w-full",
             )}
           >
             <div
@@ -236,12 +277,13 @@ export default function LotteryPage() {
                 <div
                   onMouseEnter={() => setFuelCellsInfoModal(true)}
                   onMouseLeave={() => setFuelCellsInfoModal(false)}
-                  className="flex justify-center items-center gap-[4px] text-[12px] leading-[12px] font-general-sans font-semibold uppercase underline underline-offset-2"
+                  className="flex justify-center items-center gap-[4px] text-[12px] leading-[12px] font-general-sans font-semibold uppercase"
                 >
-                  {/* <PiTrophyDuotone className="text-[16px]" /> */}
+                  <PiTrophyDuotone className="text-[16px]" />
                   <span>{fuelCellsWon} Fuel cells won</span>
-                  <PiInfoDuotone className="text-[16px]" />
-                  <AnimatePresence>
+                  {/* <PiInfoDuotone className="text-[16px]" /> */}
+                  {/* hidden for a while since implementation is underway  */}
+                  {/* <AnimatePresence>
                     {fuelCellsInfoModalOpening && (
                       <motion.div
                         initial={{ opacity: 0 }}
@@ -281,7 +323,7 @@ export default function LotteryPage() {
                         </div>
                       </motion.div>
                     )}
-                  </AnimatePresence>
+                  </AnimatePresence> */}
                 </div>
               </div>
             </div>
@@ -309,8 +351,19 @@ export default function LotteryPage() {
           </form>
           <div
             className={cn(
+              "flex flex-col justify-start items-start gap-[8px]",
+              "p-[8px] rounded-[6px]",
+              "bg-agblack/30 backdrop-blur-lg",
+              "font-extrabold",
+              "w-full",
+            )}
+          >
+            <Timer label="Next Lottery in" timestamp={nextLotteryTimestamp} />
+          </div>
+          <div
+            className={cn(
               "flex flex-col justify-start items-start gap-[8px] w-full",
-              "py-[8px] px-[16px] rounded-[6px]",
+              "px-[16px] py-[16px] rounded-[6px]",
               "bg-agblack/30 backdrop-blur-lg",
               "font-extrabold",
             )}
@@ -318,19 +371,15 @@ export default function LotteryPage() {
             <ProgressingStates
               states={lotteryState}
               journeyId={`${lotteriesInfo?.journeyId}`}
-              amountPerFuelCell={LotteryAdditionalInfo.amountPerFuelCell}
             />
           </div>
-          <div
-            className={cn(
-              "flex flex-col justify-start items-start gap-[8px]",
-              "p-[8px] rounded-[6px]",
-              "bg-agblack/30 backdrop-blur-lg",
-              "font-extrabold",
-            )}
-          >
-            <Timer label="Next Lottery in" timestamp={nextLotteryTimestamp} />
-          </div>
+
+          <Table
+            header={tableConfig.header}
+            body={tableConfig.data}
+            className="block lg:hidden"
+            bodyClassName="text-center flex justify-center items-center"
+          />
         </div>
       </div>
     </div>
