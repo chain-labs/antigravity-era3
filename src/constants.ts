ea0c94df86d78e87d4c4b0ca8aceb5a08cc0568b
Angel Lakra <angel.lakra@chainlabs.in>
2024-11-22 23:06:37 +0530

    updated contracts

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const IMAGEKIT = "https://ik.imagekit.io/qvs5pk2dl/Antigravity";
+export const IMAGEKIT = `${process.env.NEXT_PUBLIC_IMAGEKIT_BASE_URL}`;
 export const SUBGRAPH_URL = `${process.env.NEXT_PUBLIC_SUBGRAPH_URL}`;
 export const API_ENDPOINT = `${process.env.NEXT_PUBLIC_API_ENDPOINT}`;
 export const ALCHEMY_KEY = `${process.env.NEXT_PUBLIC_ALCHEMY_KEY}`;
@@ -9,7 +9,7 @@ export const EVIL_ADDRESS_AVAILABLE =
 export const UNWRAP_AVAILABLE =
   process.env.NEXT_PUBLIC_UNWRAP_AVAILABLE === "true";
 
-export const AGPROJECT_LINK = "https://test.agproject.io";
+export const AGPROJECT_LINK = `${process.env.NEXT_PUBLIC_PROD_SITE}`; // PROD-TODO: update this
 
 export const BACKGROUNDS = {
   LOTTERY: process.env.NEXT_PUBLIC_LOTTERY_BG,
